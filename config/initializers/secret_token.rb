1b1fd8b69e4efe75b9d1f2ac514c9771a3f2a7c0
ben tillett <benjamin.tillett@gmail.com>
2014-02-11 16:10:24 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GravyTrain::Application.config.secret_token = 'cce3673a9ea15ce526e021a39b24beb72c60057848cb6b5ca73f5b17277ab4d9d5b69e9810b3c142ae3d1cc20fa8646eaa7fe19194e7faed21fad2fe5877bdbf'
